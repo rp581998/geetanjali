57fa037528dcb4db1e3b6b10fb3d33cb20a168de
Rajan Patel <rp581988@gmail.com>
2021-05-03 22:07:27 +0530

    initial project scaffolding

---
+++ src/components/pages/Profile.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div className="container" >
+            <div className="py-4" >
+            <h1>Profile page</h1>
+            </div>
+        </div>
+    );
+};
+
+export default Profile;
